f9e590d9ab5d224a35888560a92235df7aaafe98
GHJhonatan <166961380+GHJhonatan@users.noreply.github.com>
2026-09-20 21:11:03 -0500

    Initial commit o Estructura inicial del proyecto

---
+++ BackEndIntelliJGrupo4/src/main/java/com/upc/backendintellijgrupo4/BackEndIntelliJGrupo4Application.java
@@ -0,0 +1,13 @@
+package com.upc.backendintellijgrupo4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndIntelliJGrupo4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndIntelliJGrupo4Application.class, args);
+    }
+
+}
---
+++ BackEndIntelliJGrupo4/src/test/java/com/upc/backendintellijgrupo4/BackEndIntelliJGrupo4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.backendintellijgrupo4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndIntelliJGrupo4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
